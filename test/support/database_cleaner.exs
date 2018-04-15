b8060411fba93c34d36e016aed877102466ea359
Leonardo Pires <leonardo.pires@gmail.com>
2018-04-15 07:28:22 -0300

    Use basic authentication in the tests

--- test/support/database_cleaner.exs
+++ test/support/database_cleaner.exs
@@ -1,7 +1,13 @@
 defmodule DatabaseCleaner do
   def ensure_clean_db!(repo, schema) do
     config = Application.get_env(:couchdb_adapter, repo)
-    server = :couchbeam.server_connection(config[:hostname], config[:port], "", [])
+    opts =
+      if config[:user] && config[:password] do
+        [{:basic_auth, {config[:user], config[:password]}}]
+      else
+        []
+      end
+    server = :couchbeam.server_connection(config[:hostname], config[:port], "", opts)
     db_name = schema.__schema__(:source)
     if :couchbeam.db_exists(server, db_name) do
       :couchbeam.delete_db(server, db_name)
