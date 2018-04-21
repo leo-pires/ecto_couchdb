b5786fe36b8e3d029fc35e9423c5d402ab8644d5
Leonardo Pires <leonardo.pires@gmail.com>
2018-04-21 20:50:09 -0300

    Change database config

--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,11 @@ use Mix.Config
 
 config :couchdb_adapter, Repo,
   adapter: CouchdbAdapter,
+  protocol: "http",
   hostname: "localhost",
   port: 5984,
+  user: "admin",
+  password: "admin",
+  database: "ecto_couchdb_test",
   pool_size: 5,
   pool_timeout: 2000
