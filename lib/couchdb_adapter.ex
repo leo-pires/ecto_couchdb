083309238e17507cf6a28557b709ea7b18b25729
Leonardo Pires <leonardo.pires@gmail.com>
2018-04-23 18:21:05 -0300

    Remove queries code

--- lib/couchdb_adapter.ex
+++ lib/couchdb_adapter.ex
@@ -11,7 +11,6 @@ defmodule CouchdbAdapter do
   def autogenerate(:id),        do: nil
   def autogenerate(:embed_id),  do: Ecto.UUID.generate()
   def autogenerate(:binary_id), do: nil
-  # def autogenerate(:string), do: nil
 
   @doc false
   def loaders({:embed, _} = type, _), do: [&load_embed(type, &1)]
@@ -32,7 +31,6 @@ defmodule CouchdbAdapter do
   defp atomize_keys({map}), do: atomize_keys(map)
   defp atomize_keys(map), do: for {k, v} <- map, do: {String.to_atom(k), v}
 
-  @doc false
   def dumpers(:naive_datetime, type), do: [type, &dump_naive_datetime/1]
   def dumpers(:date, type), do: [type, &dump_date/1]
   def dumpers(:time, type), do: [type, &dump_time/1]
@@ -50,7 +48,6 @@ defmodule CouchdbAdapter do
     config_options = Keyword.take(config, [:max_connections, :timeout])
     Keyword.merge @default_pool_options, config_options
   end
-
 
   # Returns the server connection to use with the given repo
   # TODO: reuse url_for
@@ -90,14 +87,6 @@ defmodule CouchdbAdapter do
     Application.ensure_all_started(:couchbeam, type)
   end
 
-  @doc false
-  # Dev notes:
-  # - repo: is the module from which we were called (the one using Exto.Repo)
-  # - meta: is https://hexdocs.pm/ecto/Ecto.Adapter.html#t:schema_meta/0
-  # - fields: is a Keyword.t with field-value pairs
-  # - on_conflict: https://hexdocs.pm/ecto/Ecto.Adapter.html#t:on_conflict/0
-  # - returning: list of atoms of fields whose value needs to be returned
-  # - options: ??? Seems to be a Keyword.t (but the actual type is options). Arrives as [skip_transaction: true]
   def insert(repo, meta, fields, _on_conflict, returning, _options) do
     type = db_name(meta)
     database = repo.config[:database]
@@ -114,7 +103,6 @@ defmodule CouchdbAdapter do
     end
   end
 
-  @doc false
   def insert_all(repo, schema_meta, _header, list, _on_conflict, returning, _options) do
     type = db_name(schema_meta)
     database = repo.config[:database]
@@ -163,80 +151,13 @@ defmodule CouchdbAdapter do
     {field_name, value}
   end
 
-  @doc false
-  def prepare(:all, query), do: {:nocache, normalize_query(query)}
-  def prepare(:delete_all, _), do: raise "Unsupported operation: delete_all"
-
-  defp normalize_query(%{joins: [_|_]}), do: raise "joins are not supported"
-  defp normalize_query(%{preloads: [_|_]}), do: raise "preloads are not supported"
-  defp normalize_query(%{havings: [_|_]}), do: raise "havings are not supported"
-  defp normalize_query(%{distinct: d}) when d != nil, do: raise "distinct is not supported"
-  defp normalize_query(%{select: %{expr: {:&, _, [0]},
-                                   fields: [{:&, _, [0, _fields, _]}]},
-                         sources: {{_db_name, schema}}
-                        } = query) do
-    {view, options} = process_wheres(query.wheres, schema)
-    %{view: view, options: [include_docs: true] ++ options}
+  def prepare(:all, _) do
+    raise "Unsupported operation in CouchdbAdapter: all"
   end
-  defp normalize_query(query) do
-    raise "Unsupported query: #{inspect Map.from_struct query}"
+  def prepare(:delete_all, _) do
+    raise "Unsupported operation in CouchdbAdapter: delete_all"
   end
 
-  defp process_wheres([], schema), do: {{schema.__schema__(:default_design), "all"}, []}
-  defp process_wheres([where], schema), do: process_where(where, schema)
-  defp process_wheres(wheres, _schema), do: raise("Only 1 where clause is allowed: #{inspect wheres}")
-
-  defp process_where(%Ecto.Query.BooleanExpr{op: :and, expr: expr}, schema) do
-    process_and_expr(expr, schema)
-  end
-  defp process_where(expr, _), do: raise "Unsupported where clause: #{inspect expr}"
-
-  defp process_and_expr(expr, schema) do
-    case expr do
-      {:==, _ , [lhs, rhs]} -> process_eq(lhs, rhs, schema)
-      {:in, _ , [lhs, rhs]} -> process_in(lhs, rhs, schema)
-      {:>=, _ , [lhs, rhs]} -> process_gt(lhs, rhs, schema)
-      {:<=, _ , [lhs, rhs]} -> process_lt(lhs, rhs, schema)
-      {:and, _, exprs} -> Enum.reduce(exprs, {nil, []}, &merge_and_expressions(&1, &2, schema))
-      _ -> raise "Unsupported expression: #{inspect expr}"
-    end
-  end
-
-  defp merge_and_expressions(expr, {view, opts}, schema) do
-    {new_view, new_opts} = process_and_expr(expr, schema)
-    if view && view != new_view do
-      raise("""
-            Detected view #{inspect view}, but got #{inspect new_view}
-            from expression #{inspect expr}
-            """)
-    else
-      {new_view, Keyword.merge(opts, new_opts, &solve_and_opts_conflict/3)}
-    end
-  end
-
-  defp solve_and_opts_conflict(key, current_value, new_value)
-  # Combine multiple in by using the intersection of keys
-  defp solve_and_opts_conflict(:keys, current, new), do: current -- (current -- new)
-  defp solve_and_opts_conflict(key, current, new) do
-    raise("Tried to assign #{inspect new} to #{inspect key}, with current value #{inspect current}")
-  end
-
-  defp process_eq({{:., _, [{:&, _, [0]}, view]}, _, _}, rhs, schema) do
-    {{schema.__schema__(:default_design), to_string(view)}, key: rhs}
-  end
-
-  defp process_in({{:., _, [{:&, _, [0]}, view]}, _, _}, rhs, schema) do
-    {{schema.__schema__(:default_design), to_string(view)}, keys: rhs}
-  end
-
-  defp process_gt({{:., _, [{:&, _, [0]}, view]}, _, _}, rhs, schema) do
-    {{schema.__schema__(:default_design), to_string(view)}, startkey: rhs}
-  end
-  defp process_lt({{:., _, [{:&, _, [0]}, view]}, _, _}, rhs, schema) do
-    {{schema.__schema__(:default_design), to_string(view)}, endkey: rhs}
-  end
-
-  @doc false
   def delete(repo, _schema_meta, filters, _options) do
     database = repo.config[:database]
     with server <- server_for(repo),
@@ -255,35 +176,8 @@ defmodule CouchdbAdapter do
     end
   end
 
-  @doc false
-  def execute(repo, query_meta, {_cache, query}, _params, preprocess, _options) do
-    type = db_name(query_meta)
-    database = repo.config[:database]
-    with server <- server_for(repo),
-         {:ok, db} <- :couchbeam.open_db(server, database),
-         {:ok, data} <- :couchbeam_view.fetch(db, query.view, query.options)
-    do
-      {records, count} = Enum.map_reduce(data, 0, &{process_result(&1, preprocess, query_meta.fields), &2 + 1})
-      {count, records}
-    else
-      {:error, {:error, reason}} -> raise inspect(reason)
-      {:error, reason} -> raise "Error while searching (#{inspect(reason)})"
-    end
-  end
-
-  defp process_result(record, process, ast) do
-    case doc = :couchbeam_doc.get_value("value", record) do
-      :undefined -> raise "Document not found on result: #{inspect record}"
-      _ -> process_doc(doc, process, ast)
-    end
-  end
-
-  defp process_doc(:undefined, _, _), do: :undefined
-  defp process_doc(doc, process, ast) do
-    Enum.map(ast, fn {:&, _, [_, fields, _]} = expr when is_list(fields) ->
-      data = fields |> Enum.map(&:couchbeam_doc.get_value(to_string(&1), doc, nil))
-      process.(expr, data, nil)
-    end)
+  def execute(_repo, _query_meta, _, _params, _preprocess, _options) do
+    raise "Unsupported operation in CouchdbAdapter: execute"
   end
 
   def update(repo, schema_meta, fields, filters, returning, _options) do
